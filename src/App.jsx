9057518169167e258549ed27cdb7a4aa1f5bc6e6
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-03-25 20:13:44 -0300

    feat: add i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 function App() {
-  
+ const { t, i18n: {changeLanguage, language} } = useTranslation();
+ const [currentLanguage, setCurrentLanguage] = useState(language)
+ const handleChangeLanguage = () => {
+   const newLanguage = currentLanguage === "en" ? "pt" : "en";
+   setCurrentLanguage(newLanguage);
+   changeLanguage(newLanguage);
+ }
+ 
+ return (     
+    <div>
+      <p>{t("key")}</p> 
+      <p>Current Language: {currentLanguage}</p>     
+      <button 
+        type="button" 
+        onClick={handleChangeLanguage}
+      >
+        Change Language
+      </button>
+    </div>
+ );
 }
 
 export default App;

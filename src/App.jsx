dc66029b884ebaf965059bd4c1289eb03e6d5832
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-03-27 14:46:34 -0300

    feat: style contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Copy } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import Nav from "./components/sections/Nav";
 import Icon from "./components/Icon";
@@ -6,19 +5,7 @@ import About from "./components/sections/About";
 import Skills from "./components/sections/Skills";
 import Experience from "./components/sections/Experience";
 import Projects from "./components/sections/Projects";
-
-function copyClipboard(text) {
-  return () => {
-    navigator.clipboard.writeText(text);
-  }
-}
-
-const contacts = [
-  "ottonellicopattibruno@gmail.com",
-  "https://github.com/brunocopatti",
-  "https://linkedin.com/in/brunocopatti",
-  "https://brunocopatti.github.io"
-]
+import Contact from "./components/sections/Contact";
 
 function App() {
  const { t, i18n: {changeLanguage, language} } = useTranslation();
@@ -45,22 +32,9 @@ function App() {
       <div className="max-w-5xl m-auto mb-6">
         <Projects />
       </div>
-      <section id="contact">
-        <h2>{t("Contact me")}</h2>
-        <ul>
-          {contacts.map((contact) => (
-            <li key={contact}>
-              <span>{contact}</span>
-              <button className="hover:cursor-pointer" onClick={copyClipboard(contact)}>
-                <Copy />
-                <span className="sr-only">{t("Copy to clipboard")}</span>
-              </button>
-            </li>
-          ))}
-        </ul>
-        <p>{t("contact_description")}</p>
-        <a href="mailto:ottonellicopattibruno@gmail.com">{t("Mail me")}</a>
-      </section>
+      <div className="max-w-5xl m-auto mb-6">
+        <Contact />
+      </div>
       <footer>
         <p>{t("Made by")} <a href="https://brunocopatti.github.io">Bruno Copatti</a></p>
         <ul>
---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,67 @@
+import { Copy } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import ShowOnVisible from "../ShowOnVisible";
+
+function copyClipboard(text) {
+  return () => {
+    navigator.clipboard.writeText(text);
+  }
+}
+
+const contacts = [
+  {
+    display: "ottonellicopattibruno@gmail.com",
+    link: "mailto:ottonellicopattibruno@gmail.com"
+  },
+  {
+    display: "github.com/brunocopatti",
+    link: "https://github.com/brunocopatti"
+  },
+  {
+    display: "linkedin.com/in/brunocopatti",
+    link: "https://linkedin.com/in/brunocopatti"
+  },
+  {
+    display: "brunocopatti.github.io",
+    link: "https://brunocopatti.github.io"
+  }
+];
+
+function Contact() {
+	const { t } = useTranslation();
+
+	return (
+		<section className="p-3" id="contact">
+			<ShowOnVisible>
+				<h2 className="text-cyan-700 mb-1">{t("Contact me")}</h2>
+				<div className="flex flex-col gap-5">
+					<ul>
+						{contacts.map((contact) => (
+							<ShowOnVisible>
+								<li className="flex max-w-96 gap-3 justify-between items-center" key={contact}>
+									<a
+										className="flex-1 text-ellipsis overflow-hidden text-lg"
+										href={contact.link}
+									>
+										{contact.display}
+									</a>
+									<button className="hover:cursor-pointer" onClick={copyClipboard(contact.link)}>
+										<Copy />
+										<span className="sr-only">{t("Copy to clipboard")}</span>
+									</button>
+								</li>
+							</ShowOnVisible>
+						))}
+					</ul>
+					<p>{t("contact_description")}</p>
+					<a
+						className="py-4 px-8 w-fit border rounded-xl font-semibold bg-cyan-700 hover:bg-cyan-950"
+						href="mailto:ottonellicopattibruno@gmail.com"
+					>{t("Mail me")}</a>
+				</div>
+			</ShowOnVisible>
+		</section>
+	);
+}
+
+export default Contact;

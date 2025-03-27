d5dabe3213d34dd898d055ba421a0932a69308ec
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-03-27 11:49:13 -0300

    feat: add load element animation

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,13 +1,21 @@
+import { useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { FileUser } from "lucide-react";
 import Profile from "../../assets/profile.jpeg";
 import Icon from "../Icon";
+import useIsVisible from "../../hooks/useIsVisible";
 
 function About() {
 	const { t } = useTranslation();
+	const ref = useRef();
+	const isVisible = useIsVisible(ref);
 
 	return (
-		<section className="p-3" id="about">
+		<section
+			ref={ref}
+			className={`p-3 ${isVisible ? "load-element" : ""}`}
+			id="about"
+		>
 			<h2 className="text-cyan-700 mb-1">{t("About me")}</h2>
 			<div className="flex items-center gap-6 not-lg:flex-col">
 				<div className="flex flex-col gap-6 not-lg:items-center">
